2d4f77af85fc0d44165b93fd1b63525a761f1b80
jacobwright <jacob.thewrightone@gmail.com>
2024-04-02 11:23:31 -0500

    read from file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # js_MergeSort
+
+1. Read an input from input.json containing a collection of integer arrays
+    1. If no input exists, create a file called input.json
+    1. Write 50 arrays of 100 random integers into the file
+    1. Return the arrays 
+1. Merge Sort the arrays
+1. Save the results into a file called results.json
--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-console.log("Hello World!");
+const fs = require('node:fs/promises');
+// console.log("Hello World!");
 
-
+const fileLocation = 'C:\\Users\\Jacob\\GitHub_Projects\\nodejs\\js_MergeSort\\input.json';
+var data = [[1, 2, 3],[4, 5, 6]];
+fs.readFile(fileLocation)
+    .catch(e => {
+        console.log(`couldn't read from file because ${e}`);
+        let temp = JSON.stringify([[7, 8,9],[0, 1, 2]]);
+        fs.writeFile(fileLocation, temp);
+        return temp;
+    })
+    .then(res => {
+        data = JSON.parse(res);
+        console.log(data);
+    });
 
 
