65436c603e307a3192c91b639e5d7f9cfc25b53c
jacobwright <jacob.thewrightone@gmail.com>
2024-04-02 11:30:50 -0500

    tools file

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const fs = require('node:fs/promises');
+const tools = require('./tools');
 // console.log("Hello World!");
 
 const fileLocation = 'C:\\Users\\Jacob\\GitHub_Projects\\nodejs\\js_MergeSort\\input.json';
-var data = [[1, 2, 3],[4, 5, 6]];
-fs.readFile(fileLocation)
+var data = fs.readFile(fileLocation)
     .catch(e => {
         console.log(`couldn't read from file because ${e}`);
-        let temp = JSON.stringify([[7, 8,9],[0, 1, 2]]);
+        let temp = JSON.stringify(tools.createData());
         fs.writeFile(fileLocation, temp);
         return temp;
     })
     .then(res => {
-        data = JSON.parse(res);
-        console.log(data);
+        return JSON.parse(res);
     });
 
+data.then((res) => {
+    console.log(res);
+})
 
---
+++ tools.js
@@ -0,0 +1,5 @@
+module.exports = {
+    createData: function() {
+        return [[7, 8,9],[0, 1, 2]];
+    }
+}
